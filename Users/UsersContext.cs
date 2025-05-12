35fc3c3d2faf301eba8e7495d390cd4c3dc1cf6f
VadimMaximovv <93405971+VadimMaximovv@users.noreply.github.com>
2025-05-12 23:40:44 +0300

    Pre-creation of the admin

--- Users/UsersContext.cs
+++ Users/UsersContext.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-
 namespace ATON_APIQuest.Users
 {
     public class UsersContext : DbContext
     {
-        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options) 
         {
+            var admin = new User
+            {
+                Login = "Admin",
+                Password = "Admin123",
+                Name = "System Administrator",
+                Gender = 1,
+                Birthday = null,
+                Admin = true,
+                CreatedBy = "System"
+            };
+
+            Users.AddAsync(admin);
+            SaveChangesAsync();
         }
 
         public DbSet<User> Users { get; set; } = null!;
