9f6dabc4172bb45517ff36eade71d9c7270796b6
VadimMaximovv <93405971+VadimMaximovv@users.noreply.github.com>
2025-05-12 15:11:51 +0300

    Writing API

---
+++ Users/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ATON_APIQuest.Users;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Login { get; set; }
+    public required string Password { get; set; }
+    public required string Name { get; set; }
+    public int Gender { get; set; } // 0 - female, 1 - male, 2 - unknown
+    public DateTime? Birthday { get; set; }
+    public bool Admin { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public required string CreatedBy { get; set; }
+    public DateTime? ModifiedOn { get; set; }
+    public string? ModifiedBy { get; set; }
+    public DateTime? RevokedOn { get; set; }
+    public string? RevokedBy { get; set; }
+
+    public bool IsActive => RevokedOn == null;
+}
---
+++ Users/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace ATON_APIQuest.Users
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
